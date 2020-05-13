dcdf56e72ecc5e2dacf05945e4bbe623ed2d1840
Bin <m13678954738@gmail.com>
2020-05-13 09:51:16 +0800

    Your first django-powered page - Your first URLconf - Your second view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from mysite.view import hello
+from mysite.view import hello, current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^hello/$', hello),
+    path('hello/', hello),
+    path('time/', current_datetime),
 ]
