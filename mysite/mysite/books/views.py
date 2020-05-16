bbe19d5e90db98fae8091fa0bbfc6f5f9ec7e479
Bin <m13678954738@gmail.com>
2020-05-16 10:57:23 +0800

    Forms - Getting Data from the Request Object - A simple Form-Handling example

--- mysite/mysite/books/views.py
+++ mysite/mysite/books/views.py
@@ -1,3 +1,18 @@
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from mysite.books.models import Book
+
 from django.shortcuts import render
 
-# Create your views here.
+def search_form(request):
+    renderer =  render_to_string('search_form.html')
+    return HttpResponse(renderer)
+
+def search(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        books = Book.objects.filter(title__contains=q)
+        renderer = render_to_string('search_results.html', {'books':books, 'query':q})
+    else:
+        renderer = 'Please submit a search item'
+    return HttpResponse(renderer)
